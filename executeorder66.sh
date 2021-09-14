67c22c17e933a0c6acac092ad64d812d201f2f0e
Niklas Fransson <ceniklas@gmail.com>
2021-09-14 16:15:13 +0200

    Update executeorder66.sh

--- executeorder66.sh
+++ executeorder66.sh
@@ -2,7 +2,7 @@ touch ~/.executeorder66.function
 
 executeorder66command='echo "Wipe them out."
 git fetch -p && for x in $(git for-each-ref --format '\''%(refname) %(upstream:track)'\'' refs/heads | awk '\''$2 == "[gone]" {sub("refs/heads/", "", $1); print $1}'\''); do git branch -D $x; done
-cowsay -f vader "Yes my master."'
+cowsay -f vader "Yes my master."' > ~/.executeorder66.function
 
 echo $executeorder66command > ~/.executeorder66.function
 
