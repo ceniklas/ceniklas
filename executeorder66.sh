8ee83d29491504f3e440ac694d489b81c269a55b
Niklas Fransson <ceniklas@gmail.com>
2021-09-14 16:47:19 +0200

    Update executeorder66.sh

--- executeorder66.sh
+++ executeorder66.sh
@@ -13,10 +13,23 @@ echo "\n" >> ~/.executeorder66.function
 
 chmod +x ~/.executeorder66.function
 
-if grep -Fq "function executeorder66" ~/.zshrc
+zshrcfunction='function executeorder66 () { ~/.executeorder66.function }'
+
+if grep -Fq zshrcfunction ~/.zshrc
 then
   echo 'executeorder66 updated to latest and greatest!'
+elif grep -Fq "function executeorder66" ~/.zshrc
+then
+  echo "Replacing old"
+  sed -i '' 's+function executeorder66.*}+function executeorder66 () { ~/.executeorder66.function }+g' ~/.zshrc
 else
-  echo 'function executeorder66 () { ~/.executeorder66.function }' >> ~/.zshrc
+  echo zshrcfunction >> ~/.zshrc
   echo 'executeorder66 installed!'
 fi
+
+
+# function executeorder66 () {
+#   echo 'Wipe them out.'
+#   git fetch -p && for x in $(git for-each-ref --format '%(refname) %(upstream:track)' refs/heads | awk '$2 == "[gone]" {sub("refs/heads/", "", $1); print $1}'); do git branch -D $x; done
+#   cowsay -f vader "Yes my master"
+# }
