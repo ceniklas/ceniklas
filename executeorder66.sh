5b01418c237176530e41255cbac94053d0ddc52c
Niklas 'Nisse' Fransson <niklas.fransson@getbright.se>
2022-04-07 17:06:35 +0200

    order66 install update

--- executeorder66.sh
+++ executeorder66.sh
@@ -1,32 +1,34 @@
+#!/bin/bash
+
 touch ~/.executeorder66.function
 
-executeorder66welcome='echo "Wipe them out."'
+executeorder66welcome='echo "Wipe them out. All of them."'
 executeorder66git='git fetch -p && for x in $(git for-each-ref --format '\''%(refname) %(upstream:track)'\'' refs/heads | awk '\''$2 == "[gone]" {sub("refs/heads/", "", $1); print $1}'\''); do git branch -D $x; done'
-executeorder66bye='cowsay -f vader "Yes my master."'
+executeorder66bye='cowsay -f vader "Yes master."'
 
-echo $executeorder66welcome > ~/.executeorder66.function
-echo "\n" >> ~/.executeorder66.function
-echo $executeorder66git >> ~/.executeorder66.function
-echo "\n" >> ~/.executeorder66.function
-echo $executeorder66bye >> ~/.executeorder66.function
-echo "\n" >> ~/.executeorder66.function
+echo $executeorder66welcome >~/.executeorder66.function
+echo "\n" >>~/.executeorder66.function
+echo $executeorder66git >>~/.executeorder66.function
+echo "\n" >>~/.executeorder66.function
+echo $executeorder66bye >>~/.executeorder66.function
+echo "\n" >>~/.executeorder66.function
 
 chmod +x ~/.executeorder66.function
 
-zshrcfunction='function executeorder66 () { ~/.executeorder66.function }'
+profilefunction='function executeorder66 () { ~/.executeorder66.function }'
 
-if grep -Fq zshrcfunction ~/.zshrc
-then
-  echo 'executeorder66 updated to latest and greatest!'
-elif grep -Fq "function executeorder66" ~/.zshrc
-then
-  echo "Replacing old"
-  sed -i '' 's+function executeorder66.*}+function executeorder66 () { ~/.executeorder66.function }+g' ~/.zshrc
-else
-  echo zshrcfunction >> ~/.zshrc
-  echo 'executeorder66 installed!'
+if [ -n "$($SHELL -c 'echo $ZSH_VERSION')" ]; then
+  # assume Zsh
+  if grep -Fq "function executeorder66" ~/.zshrc; then
+    echo 'executeorder66 updated to latest and greatest!'
+  else
+    echo $profilefunction >>~/.zshrc
+    echo 'executeorder66 installed!'
+  fi
+elif [ -n "$($SHELL -c 'echo $BASH_VERSION')" ]; then
+  # assume Bash
+  echo "\nBash is untested, but feel free to add\n\n    $profilefunction \n\nto ~/.bash_profile, ~/.bashrc or whatever you're using.\n"
 fi
-
 
 # function executeorder66 () {
 #   echo 'Wipe them out.'
